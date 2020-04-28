b52fad18d1abf84ae0c5a965fa3f3b2fe98dbcf8
Alejandro Corbellini <alejandrocorbellini@gmail.com>
2020-04-27 23:40:14 -0300

    fix styles

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+window.Popper = Popper;
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
--- client/src/app/todoList/todolist.component.ts
+++ client/src/app/todoList/todolist.component.ts
@@ -8,64 +8,68 @@ import { TodoService } from './todo.service';
 declare var jQuery: any;
 
 @Component({
-    selector: 'todo-list',
-    templateUrl: './todolist.component.html',
-    providers: [TodoService, UserService]
+  selector: 'todo-list',
+  templateUrl: './todolist.component.html',
+  providers: [TodoService, UserService],
 })
-
 export class TodoListComponent implements AfterViewChecked {
-    todos: Todo[];
-    user: User;
-    todoContent: String;
-    interval: any;
-    constructor(private todoService: TodoService, private router: Router, private userService: UserService) { };
+  todos: Todo[];
+  user: User;
+  todoContent: String;
+  interval: any;
+  constructor(
+    private todoService: TodoService,
+    private router: Router,
+    private userService: UserService
+  ) {}
 
-    ngOnInit() {
-        this.userService.getCurrentUser().then(user => {
-            this.checkUser(user)
-        });
-    }
+  ngOnInit() {
+    this.userService.getCurrentUser().then((user) => {
+      this.checkUser(user);
+    });
+  }
 
-    public checkUser(user: User): void {
-        this.user = user;
-        if (!this.user.id) {
-            this.router.navigate(["/login"]);
-        } else {
-            this.update();
-            this.interval = setInterval(() => {
-                this.update();
-            }, 10000);
-        }
+  public checkUser(user: User): void {
+    this.user = user;
+    if (!this.user.id) {
+      this.router.navigate(['/login']);
+    } else {
+      this.update();
+      this.interval = setInterval(() => {
+        this.update();
+      }, 10000);
     }
+  }
 
-    update() {
-        this.todoService.getTodos().then(todos => {
-            this.todos = todos;
-        });
-    }
+  update() {
+    this.todoService.getTodos().then((todos) => {
+      this.todos = todos;
+    });
+  }
 
-    ngAfterViewChecked() {
-        jQuery(".delete-todo-button").tooltip();
-    }
+  ngAfterViewChecked() {
+    jQuery('.delete-todo-button').tooltip();
+  }
 
-    nuevoTodo() {
-        this.todoContent = "";
-        jQuery("#addTodo").modal('show');
-        setTimeout(function() {
-          jQuery("#todoContent").focus();
-        }, 500);
-    }
+  nuevoTodo() {
+    this.todoContent = '';
+    jQuery('#addTodo').modal('show');
+    setTimeout(function () {
+      jQuery('#todoContent').focus();
+    });
+  }
 
-    guardarTodo() {
-        this.todoService.saveTodo(this.todoContent).then(() => {
-            jQuery("#addTodo").modal('hide');
-            this.update();
-        });
-    }
+  guardarTodo() {
+    this.todoService.saveTodo(this.todoContent).then(() => {
+      jQuery('#addTodo').modal('hide');
+      this.update();
+    });
+  }
 
-    borrarTodo(todo: Todo){
-      this.todoService.deleteTodo(todo).then(() => {
-          this.update();
-      });
-    }
+  borrarTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo).then(() => {
+      jQuery('.delete-todo-button').tooltip('hide');
+      this.update();
+    });
+  }
 }
