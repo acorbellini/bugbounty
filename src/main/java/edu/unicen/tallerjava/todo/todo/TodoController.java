dc1205ef7de9731305001c918825d315b4b7f329
Alejandro Corbellini <alejandrocorbellini@gmail.com>
2020-04-27 22:41:14 -0300

    change delete todo to receive id

--- src/main/java/edu/unicen/tallerjava/todo/todo/TodoController.java
+++ src/main/java/edu/unicen/tallerjava/todo/todo/TodoController.java
@@ -2,31 +2,33 @@ package edu.unicen.tallerjava.todo.todo;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import javax.websocket.server.PathParam;
 
-import edu.unicen.tallerjava.todo.users.CurrentUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class TodoController {
     @Autowired
-    private
-    TodoService svc;
+    private TodoService svc;
 
     @GetMapping(value = "/api/todo", produces = "application/json")
-    public @ResponseBody
-    Iterable<ToDo> getTodoList() {
+    public @ResponseBody Iterable<ToDo> getTodoList() {
         return svc.getTodoList();
     }
 
-    @DeleteMapping(value = "/api/todo", produces = "application/json")
-    public void deleteTodo(@RequestBody ToDo todo) {
-        svc.delete(todo.getId());
+    @DeleteMapping(value = "/api/todo/:id", produces = "application/json")
+    public void deleteTodo(@PathParam("id") UUID id) {
+        svc.delete(id);
     }
 
     @PostMapping(value = "/api/todo", produces = "application/json")
-    public @ResponseBody
-    ToDo addTodo(@RequestBody ToDo todo) {
+    public @ResponseBody ToDo addTodo(@RequestBody ToDo todo) {
         return svc.createNewTodo(todo.getContent());
     }
 }
